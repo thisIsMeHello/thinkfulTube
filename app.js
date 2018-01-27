780da63605a073eea6e5817b3177256c6acc4cbf
Marcus Richmond <marcu5@countermail.com>
2018-01-27 14:35:57 -0500

    most functionality in place

---
+++ app.js
@@ -0,0 +1,65 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+//creates the query object to be sent
+function getDataFromApi(searchTerm, callback) {
+  const query = {
+    maxResults: 5,
+    part: 'snippet',
+    key: 'AIzaSyBq5kuZ-vFnYlPDd4ikXepEDYYPEsk6cd4',
+    q: `${searchTerm}`,
+    type: 'video',
+  };
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+function renderResult(result) {
+  console.log(result);
+  $('.js-search-results').empty();
+  for (let i=0; i<5; i++){
+    let firstThumbnail = result.items[i].snippet.thumbnails.medium.url;
+    let thumbID = result.items[i].id.videoId;
+    let channelID = result.items[i].snippet.channelId;
+    let channelTitle = result.items[i].snippet.channelTitle;
+    let thumbnailHtml = `
+      <figure class="returnedResult">
+          <a class="thumbnail" href="https://youtube.com/watch?v=${thumbID}"><img src="${firstThumbnail}"></a>
+            <figcapation>
+              <a class="moreFrom" href="https://youtube.com/channel/${channelID}">More from ${channelTitle}</a>
+            </figcapation>  
+      </figure>
+    `;
+    $('.js-search-results').append(thumbnailHtml);
+  }
+}
+
+function renderButtons(){
+  $(".js-buttons").html(`
+      <button class="previousPage">previous page</button>
+      <button class="nextPage">next page</button>
+    `);
+}
+
+$('.js-buttons').on("click", ".nextPage", function(event){
+  console.log("next button clicked");
+})
+
+$('.js-buttons').on("click", ".previousPage", function(event){
+  console.log("previous button clicked");
+})
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    console.log(query);
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query, renderResult);
+    renderButtons();
+  });
+}
+
+$(watchSubmit);
+
+// .items["0"].snippet.thumbnails.high
